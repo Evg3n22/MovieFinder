e82946adf1b2c80b8193a997c1a2d1d41ceed4ad
evgen.gulevaty <evgen.gulevaty@gmail.com>
2024-12-20 14:05:07 +0200

    Set up slider

--- src/main.js
+++ src/main.js
@@ -3,16 +3,25 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from '@/router.js'
+
+import PrimeVue from 'primevue/config'
 import Carousel from 'primevue/carousel';
+
+// import 'primevue/resources/themes/saga-blue/theme.css'; // Тема (замість saga-blue можна використати іншу тему)
+// import 'primevue/resources/primevue.min.css'; // Основний CSS PrimeVue
+// import 'primeicons/primeicons.css'; // Іконки PrimeIcons
+
 import { createPinia } from 'pinia'
 import ('bootstrap/dist/css/bootstrap.min.css')
 
 const app = createApp(App)
 const pinia = createPinia()
 
+app.component('VCarousel', Carousel);
+
 app.use(router)
 app.use(pinia)
-app.component('VCarousel', Carousel)
-
+app.use(PrimeVue)
+// app.component('VCarousel', Carousel)
 
 app.mount('#app')
